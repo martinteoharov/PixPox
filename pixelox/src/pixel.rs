af3b74ac6622797120a40695b2ff26c115e1d606
martinteoharov <martin.teoharov15@gmail.com>
2022-07-12 21:59:40 +0300

    initial

---
+++ pixelox/src/pixel.rs
@@ -0,0 +1,23 @@
+// pub struct Colors {
+//     r: u8,
+//     g: u8,
+//     b: u8,
+// }
+
+// pub struct Position {
+//     x: u16,
+//     y: u16,
+// }
+
+// pub enum PixelClass {
+//     SAND,
+//     WATER,
+//     STONE,
+//     IRON,
+// }
+
+// pub struct Pixel {
+//     pub colors: Colors,
+//     pub pos: Position,
+//     pub class: PixelClass,
+// }
---
+++ sandbox/src/main.rs
@@ -0,0 +1,10 @@
+use pixelox;
+
+fn main() {
+    println!("Hello, world!");
+    //pixelox::window::initialize(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+
+    pixelox::run();
+
+    println!("Window created!");
+}
