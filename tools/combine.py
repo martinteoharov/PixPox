42648deb9ff41a6e9de55ba0c134d92b22f37311
martinteoharov <martin.teoharov15@gmail.com>
2023-05-04 12:14:35 +0100

    combine py

---
+++ tools/combine.py
@@ -0,0 +1,17 @@
+import os
+
+# Set your input and output directories
+input_directories = ["../crates/", "../examples/", "../src/"]
+output_file = "./merged-file.txt"
+
+with open(output_file, 'w') as outfile:
+    for input_directory in input_directories:
+        for dirpath, dirnames, filenames in os.walk(input_directory):
+            for filename in filenames:
+                    filepath = os.path.join(dirpath, filename)
+                    with open(filepath, 'r') as infile:
+                        outfile.write(f"{filepath} begin\n")
+                        outfile.write(infile.read())
+                        outfile.write(f"\n{filepath} end\n")
+
+
